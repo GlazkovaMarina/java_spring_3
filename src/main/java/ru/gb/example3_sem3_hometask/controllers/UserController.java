9b785c2219430ee8e404e89619d9298a51118c49
GlazkovaMarina <glazkova.mv@bk.ru>
2024-04-15 15:53:53 +0300

    add hw3

---
+++ src/main/java/ru/gb/example3_sem3_hometask/controllers/UserController.java
@@ -0,0 +1,34 @@
+package ru.gb.example3_sem3_hometask.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.gb.example3_sem3_hometask.services.RegistrationService;
+import ru.gb.example3_sem3_hometask.domain.User;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private RegistrationService service;
+
+    @GetMapping
+    public List<User> userList() {
+        return service.getDataProcessingService().getRepository().getUsers();
+    }
+
+    @PostMapping("/body")
+    public String userAddFromBody(@RequestBody User user)
+    {
+        service.getDataProcessingService().getRepository().getUsers().add(user);
+        return "User added from body!";
+    }
+    @PostMapping("/formdata")
+    public String userAddFromFormData(@RequestParam String name, @RequestParam int age, @RequestParam String email)
+    {
+        service.processRegistration(name, age, email);
+        return "User added from form-data!";
+    }
+}
